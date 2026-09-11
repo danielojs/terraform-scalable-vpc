4fea31c734782d0f52154b2d81a5067a1528760e
danielojs <danielojsandil@gmail.com>
2026-09-11 16:10:14 +0700

    added tags in bastion module sg

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -3,6 +3,10 @@ resource "aws_security_group" "this" {
   name        = "${var.name_prefix}-bastion-sg"
   description = "Security group for the bastion host"
   vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.name_prefix}-bastion-sg"
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ssh" {
