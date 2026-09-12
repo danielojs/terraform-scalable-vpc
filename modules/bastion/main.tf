aa44d8cd5cf4e2c9c427f20f5cb74837fc07c491
danielojs <danielojsandil@gmail.com>
2026-09-12 18:08:24 +0700

    updated allowed ssh cidr and egress rule

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -20,8 +20,6 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
 resource "aws_vpc_security_group_egress_rule" "all" {
   security_group_id = aws_security_group.this.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port           = 65535
   ip_protocol       = "-1"
 }
 
